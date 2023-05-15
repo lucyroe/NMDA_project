b681d1ad22daf3db03162a4f485c3b1358b4dca3
FeetAFlying <92034912+FeetAFlying@users.noreply.github.com>
2023-05-15 16:03:28 +0200

    new realign job script added

---
+++ realign_job.m
@@ -0,0 +1,17 @@
+%% PREPROCESSING 
+% STEP 1: Realignment
+% script takes as input fMRI img files and realigns them
+
+matlabbatch{1}.spm.spatial.realign.estwrite.data = final_img_data';
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.quality = 0.9;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.sep = 4;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.fwhm = 5;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.rtm = 1;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.interp = 2;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.weight = '';
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.which = [0 1];
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.interp = 4;
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.mask = 1;
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.prefix = 'r';
