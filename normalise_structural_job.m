597580eece823e18c4856848d8c13d38286d12cd
Kirkw97 <122607668+Kirkw97@users.noreply.github.com>
2023-05-15 22:00:08 +0200

    Add files via upload

---
+++ normalise_structural_job.m
@@ -0,0 +1,12 @@
+%-----------------------------------------------------------------------
+% Job saved on 13-May-2023 21:01:53 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.normalise.write.subj.def = {'C:\Users\KW\Documents\DATA\MoAEpilot\sM00223\y_sM00223_002.nii'};
+matlabbatch{1}.spm.spatial.normalise.write.subj.resample = {'C:\Users\KW\Documents\DATA\MoAEpilot\sM00223\msM00223_002.nii,1'};
+matlabbatch{1}.spm.spatial.normalise.write.woptions.bb = [-78 -112 -70
+                                                          78 76 85];
+matlabbatch{1}.spm.spatial.normalise.write.woptions.vox = [1 1 3];
+matlabbatch{1}.spm.spatial.normalise.write.woptions.interp = 4;
+matlabbatch{1}.spm.spatial.normalise.write.woptions.prefix = 'w';
