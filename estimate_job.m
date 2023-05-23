cefdf18ec201ea4e152c2164bf647742cda9e3cd
caglanur98 <86797255+caglanur98@users.noreply.github.com>
2023-05-23 11:13:30 +0200

    Add files via upload

---
+++ estimate_job.m
@@ -0,0 +1,8 @@
+%-----------------------------------------------------------------------
+% Job saved on 23-May-2023 11:08:14 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.fmri_est.spmmat = {'/Users/caglademirkan/Documents/MATLAB_NMDA/MoAEpilot1/SPM.mat'};
+matlabbatch{1}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
